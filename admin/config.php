89eece5930b7254c29f3bf79a9eb91ad51884490
Nicholas Roge <NicholasRoge@gmail.com>
2011-11-18 14:29:37 -0600

    Initial Commit;  Contains a fresh install of OpenCart;

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://127.0.0.1/admin/');
+define('HTTP_CATALOG', 'http://127.0.0.1/');
+define('HTTP_IMAGE', 'http://127.0.0.1/image/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://127.0.0.1/admin/');
+define('HTTPS_IMAGE', 'http://127.0.0.1/image/');
+
+// DIR
+define('DIR_APPLICATION', '/Applications/XAMPP/xamppfiles/htdocs/admin/');
+define('DIR_SYSTEM', '/Applications/XAMPP/xamppfiles/htdocs/system/');
+define('DIR_DATABASE', '/Applications/XAMPP/xamppfiles/htdocs/system/database/');
+define('DIR_LANGUAGE', '/Applications/XAMPP/xamppfiles/htdocs/admin/language/');
+define('DIR_TEMPLATE', '/Applications/XAMPP/xamppfiles/htdocs/admin/view/template/');
+define('DIR_CONFIG', '/Applications/XAMPP/xamppfiles/htdocs/system/config/');
+define('DIR_IMAGE', '/Applications/XAMPP/xamppfiles/htdocs/image/');
+define('DIR_CACHE', '/Applications/XAMPP/xamppfiles/htdocs/system/cache/');
+define('DIR_DOWNLOAD', '/Applications/XAMPP/xamppfiles/htdocs/download/');
+define('DIR_LOGS', '/Applications/XAMPP/xamppfiles/htdocs/system/logs/');
+define('DIR_CATALOG', '/Applications/XAMPP/xamppfiles/htdocs/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'opencart');
+define('DB_PREFIX', 'oc_');
+?>
---
+++ config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://127.0.0.1/');
+define('HTTP_IMAGE', 'http://127.0.0.1/image/');
+define('HTTP_ADMIN', 'http://127.0.0.1/admin/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://127.0.0.1/');
+define('HTTPS_IMAGE', 'http://127.0.0.1/image/');
+
+// DIR
+define('DIR_APPLICATION', '/Applications/XAMPP/xamppfiles/htdocs/catalog/');
+define('DIR_SYSTEM', '/Applications/XAMPP/xamppfiles/htdocs/system/');
+define('DIR_DATABASE', '/Applications/XAMPP/xamppfiles/htdocs/system/database/');
+define('DIR_LANGUAGE', '/Applications/XAMPP/xamppfiles/htdocs/catalog/language/');
+define('DIR_TEMPLATE', '/Applications/XAMPP/xamppfiles/htdocs/catalog/view/theme/');
+define('DIR_CONFIG', '/Applications/XAMPP/xamppfiles/htdocs/system/config/');
+define('DIR_IMAGE', '/Applications/XAMPP/xamppfiles/htdocs/image/');
+define('DIR_CACHE', '/Applications/XAMPP/xamppfiles/htdocs/system/cache/');
+define('DIR_DOWNLOAD', '/Applications/XAMPP/xamppfiles/htdocs/download/');
+define('DIR_LOGS', '/Applications/XAMPP/xamppfiles/htdocs/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'opencart');
+define('DB_PREFIX', 'oc_');
+?>
