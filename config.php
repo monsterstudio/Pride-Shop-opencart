6bac16136b3d4d2ebe1f99ba6b5f825b16618852
Nicholas A Roge <NicholasRoge@gmail.com>
2011-11-21 13:18:32 -0600

    Changed teh config.php file to correctly work across multiple systems.

--- config.php
+++ config.php
@@ -9,16 +9,16 @@ define('HTTPS_SERVER', 'http://127.0.0.1/');
 define('HTTPS_IMAGE', 'http://127.0.0.1/image/');
 
 // DIR
-define('DIR_APPLICATION', '/Applications/XAMPP/xamppfiles/htdocs/catalog/');
-define('DIR_SYSTEM', '/Applications/XAMPP/xamppfiles/htdocs/system/');
-define('DIR_DATABASE', '/Applications/XAMPP/xamppfiles/htdocs/system/database/');
-define('DIR_LANGUAGE', '/Applications/XAMPP/xamppfiles/htdocs/catalog/language/');
-define('DIR_TEMPLATE', '/Applications/XAMPP/xamppfiles/htdocs/catalog/view/theme/');
-define('DIR_CONFIG', '/Applications/XAMPP/xamppfiles/htdocs/system/config/');
-define('DIR_IMAGE', '/Applications/XAMPP/xamppfiles/htdocs/image/');
-define('DIR_CACHE', '/Applications/XAMPP/xamppfiles/htdocs/system/cache/');
-define('DIR_DOWNLOAD', '/Applications/XAMPP/xamppfiles/htdocs/download/');
-define('DIR_LOGS', '/Applications/XAMPP/xamppfiles/htdocs/system/logs/');
+define('DIR_APPLICATION', './catalog/');
+define('DIR_SYSTEM', './system/');
+define('DIR_DATABASE', './system/database/');
+define('DIR_LANGUAGE', './catalog/language/');
+define('DIR_TEMPLATE', './catalog/view/theme/');
+define('DIR_CONFIG', './system/config/');
+define('DIR_IMAGE', './image/');
+define('DIR_CACHE', './system/cache/');
+define('DIR_DOWNLOAD', './download/');
+define('DIR_LOGS', './system/logs/');
 
 // DB
 define('DB_DRIVER', 'mysql');
