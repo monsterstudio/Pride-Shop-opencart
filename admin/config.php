54625f2fd105dcc3fbefceaab7e5151055e8f26f
Christopher Reynolds <reynoldschristopher55@gmail.com>
2011-12-02 11:58:24 -0800

    commit my entire develp branch

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://10.211.55.9/opencart/admin/');
+define('HTTP_CATALOG', 'http://10.211.55.9/opencart/');
+define('HTTP_IMAGE', 'http://10.211.55.9/opencart/image/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://10.211.55.9/opencart/admin/');
+define('HTTPS_IMAGE', 'http://10.211.55.9/opencart/image/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/opencart/admin/');
+define('DIR_SYSTEM', '/var/www/opencart/system/');
+define('DIR_DATABASE', '/var/www/opencart/system/database/');
+define('DIR_LANGUAGE', '/var/www/opencart/admin/language/');
+define('DIR_TEMPLATE', '/var/www/opencart/admin/view/template/');
+define('DIR_CONFIG', '/var/www/opencart/system/config/');
+define('DIR_IMAGE', '/var/www/opencart/image/');
+define('DIR_CACHE', '/var/www/opencart/system/cache/');
+define('DIR_DOWNLOAD', '/var/www/opencart/download/');
+define('DIR_LOGS', '/var/www/opencart/system/logs/');
+define('DIR_CATALOG', '/var/www/opencart/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'opencart1');
+define('DB_PASSWORD', 'HKWAXXZjzn6CpazR');
+define('DB_DATABASE', 'opencart1');
+define('DB_PREFIX', '');
+?>
---
+++ config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://10.211.55.9/opencart/');
+define('HTTP_IMAGE', 'http://10.211.55.9/opencart/image/');
+define('HTTP_ADMIN', 'http://10.211.55.9/opencart/admin/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://10.211.55.9/opencart/');
+define('HTTPS_IMAGE', 'http://10.211.55.9/opencart/image/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/opencart/catalog/');
+define('DIR_SYSTEM', '/var/www/opencart/system/');
+define('DIR_DATABASE', '/var/www/opencart/system/database/');
+define('DIR_LANGUAGE', '/var/www/opencart/catalog/language/');
+define('DIR_TEMPLATE', '/var/www/opencart/catalog/view/theme/');
+define('DIR_CONFIG', '/var/www/opencart/system/config/');
+define('DIR_IMAGE', '/var/www/opencart/image/');
+define('DIR_CACHE', '/var/www/opencart/system/cache/');
+define('DIR_DOWNLOAD', '/var/www/opencart/download/');
+define('DIR_LOGS', '/var/www/opencart/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'opencart1');
+define('DB_PASSWORD', 'HKWAXXZjzn6CpazR');
+define('DB_DATABASE', 'opencart1');
+define('DB_PREFIX', '');
+?>
